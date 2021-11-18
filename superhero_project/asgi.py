4140d264d5d2c6a55a6f5ffd68805dc8116a8b4d
Teresa Jimenez <teresa.jimenez@wearemo.com>
2021-11-18 17:07:20 -0500

    Models added

--- superhero_project/asgi.py
+++ superhero_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for super_api project.
+ASGI config for superhero_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'super_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superhero_project.settings')
 
 application = get_asgi_application()
--- superhero_project/wsgi.py
+++ superhero_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for super_api project.
+WSGI config for superhero_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'super_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superhero_project.settings')
 
 application = get_wsgi_application()
